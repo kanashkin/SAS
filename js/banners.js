a4b8f0cdcd690076e7eb4d54c42cc9bf97e8fdb0
Alexander <kanashkin_ad@mail.ru>
2023-10-01 10:50:16 +0300

    add changes on titles and examples slider

--- js/banners.js
+++ js/banners.js
@@ -1,6 +1,12 @@
 let examplesSwiper = null
+const swiperSlides = document.querySelectorAll('.examples__banner')
 
 const createSwiper = () => {
+    swiperSlides.forEach(item => {
+        item.classList.add('swiper-slide')
+    })
+
+
     examplesSwiper = new Swiper('.swiper-examples', {
         slidesPerView: 1,
         navigation: {
@@ -15,12 +21,14 @@ const createSwiper = () => {
 
 const checkWidth = () => {
     if (window.innerWidth <= 576) {
-        console.log(1);
         createSwiper()
     } else {
         if (examplesSwiper) {
             examplesSwiper.destroy()
         }
+        swiperSlides.forEach(item => {
+            item.classList.remove('swiper-slide')
+        })
         examplesSwiper = null
     }
 }
